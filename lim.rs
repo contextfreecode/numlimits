51e5bc52b9535af783e54d09d42dc3641cea945d
Tom <tom@contextfree.info>
2022-11-26 05:44:32 -0700

    Start rust

--- lim.rs
+++ lim.rs
@@ -1,5 +1,21 @@
+use num_format::{Locale, WriteFormatted};
+
+struct Country {
+    name: String,
+    population: i32,
+}
+
 fn main() {
-    println!("Hi!");
+    let countries: &[Country] = &[
+        Country {name: "China".into(), population: 5},
+    ];    
+    let mut stdout = std::io::stdout();
+    let locale = &Locale::en;
+    for country in countries {
+        print!("{} ", country.name);
+        stdout.write_formatted(&country.population, locale).unwrap();
+        println!();
+    }
 }
 
 // [1] https://doc.rust-lang.org/std/#primitives
